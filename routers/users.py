7394a3450623e114dc30fc01f04598344b6cf3cd
Kolya9309 <97960704+Kolya9309@users.noreply.github.com>
2025-07-24 20:25:56 +0500

    Create users.py

---
+++ routers/users.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import User
+from schemas import UserCreate
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(username=user.username, password=user.password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
