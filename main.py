6d1805b3734abbb5ea54c4878474e185bd68569c
Kolya9309 <97960704+Kolya9309@users.noreply.github.com>
2025-07-24 20:04:49 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-from . import models, database
-from .auth import routes as auth_routes
+import models, database
+from auth import routes as auth_routes
+from meals import routes as meal_routes
 
 models.Base.metadata.create_all(bind=database.engine)
+meal_models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="MyAPP")
 
 app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
+app.include_router(meal_routes.router, prefix="/meals", tags=["meals"])
