2a171b7f023ef2286a12b50c230d8d0e7fabce4c
Kolya9309 <97960704+Kolya9309@users.noreply.github.com>
2025-07-24 19:34:51 +0500

    Add files via upload

--- auth/utils.py
+++ auth/utils.py
@@ -1,9 +1,8 @@
 from passlib.context import CryptContext
 from jose import jwt
 from datetime import datetime, timedelta
-import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+SECRET_KEY = "super-secret-key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
@@ -17,6 +16,8 @@ def verify_password(plain_password, hashed_password):
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.utcnow() + (
+        expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
